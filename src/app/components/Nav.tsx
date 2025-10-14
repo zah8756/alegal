65ed0950dd9476c288a862d8f419804e9938066f
Zach Harwood <320zach@gmail.com>
2025-10-13 23:28:32 -0400

    Refactor Nav component styles and improve accessibility; update global CSS variables

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Nav = () => {
 	return (
-		<nav className='bg-[#F7F5F5D9] w-full min-h-[133px] gap-4 flex flex-col sticky px-8 pt-8 pb-6'>
+		<nav className='bg-[#ffffff] w-full min-h-[133px] gap-2 flex flex-col sticky px-8 pt-8 pb-6'>
 			<div className='min-w-[950px] gap-8 flex flex-row justify-end'>
 				<span className='text-xl tracking-normal align-middle underline text-[#999999]'>
 					Support
@@ -11,25 +11,35 @@ const Nav = () => {
 					(631) 689-9700
 				</span>
 			</div>
+
 			<div className='flex flex-row justify-between min-w-[950px] items-center'>
+				{/* Logo*/}
 				<Image
 					src='/AnsweringLegalTM_Blue.png'
-					alt='Picture of the logo'
-					width={293}
+					alt='Answering Legal Logo'
+					width={393}
 					height={48}
 				/>
-				<ul className='flex flex-row gap-8 text-[#161641] font-semibold text-xl'>
-					<li>Products ⌄</li>
-					<li>About ⌄</li>
-					<li>Resources ⌄</li>
-					<li>Pricing</li>
+
+				{/* Navigation links */}
+				<ul className='flex flex-row gap-10 text-[#161641] font-semibold text-2xl'>
+					<li className='hover:text-button cursor-pointer'>Products ⌄</li>
+					<li className='hover:text-button cursor-pointer'>About ⌄</li>
+					<li className='hover:text-button cursor-pointer'>Resources ⌄</li>
+					<li className='hover:text-button cursor-pointer'>Pricing</li>
 				</ul>
-				<div className='gap-8'>
+
+				{/* Search*/}
+				<div className='gap-8 flex'>
 					<Image
 						src='/Search.svg'
 						alt='magnifying glass search icon'
-						width={32}
-						height={32}></Image>
+						width={64}
+						height={64}
+						sizes='100vh'></Image>
+					<button className='bg-button text-2xl font-semibold px-4 py-3 rounded-3xl'>
+						Try for Free
+					</button>
 				</div>
 			</div>
 		</nav>
